1c27b8a4b299a255e929c37eadfc7768a9e86b86
Antonis Zafeirelis <antonis@baud.tech>
2022-07-24 15:10:22 +0300

    Login/Registration frontend complete

---
+++ sql/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    isadmin BOOLEAN NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
