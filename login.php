dff11c726eee2d883912dab39cc33754e735b485
Antonis Zafeirelis <antonis@baud.tech>
2022-07-24 15:44:16 +0300

    Minor Visual Adjustments

--- login.php
+++ login.php
@@ -13,12 +13,12 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'web');
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
-if($link === false){
+if ($link === false) {
     die("ERROR: Could not connect to DataBase. " . mysqli_connect_error());
 }
 ?>
@@ -26,30 +26,35 @@ if($link === false){
 <html>
 
 <!-- Header -->
+
 <head>
-  <title>Corona Tracker</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Corona Tracker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <!-- Main Body -->
+
 <body class="parallax">
 
-<main>
-        <form action="login.php" method="post">
-            <h1>Sign Up to Corona Tracker</h1>
-            <div>
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username">
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password">
-            </div>
-            <button className = 'submitbutton' type="submit">Register</button>
-            <footer>Not Registered Yet? <a href="register.php">Click here</a></footer>
-        </form>
-    </main>
+    <div class='container loginform'>
+        <main>
+            <form action="login.php" method="post">
+                <h1>Sign In to Corona Tracker</h1>
+                <div>
+                    <label for="username">Username:</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <button className='submitbutton' type="submit">Log In</button>
+                <footer>Not Registered Yet? <a href="register.php">Click here</a></footer>
+            </form>
+        </main>
+    </div>
 
 </body>
+
 </html>
--- register.php
+++ register.php
@@ -36,44 +36,46 @@ if ($link === false) {
 <!-- Main Body -->
 
 <body class="parallax">
-    <main>
-        <form action="register.php" method="post">
-            <h1>Sign Up to Corona Tracker</h1>
-            <div>
-                <label for="username">Username:</label>
-                <input type="text" name="username" id="username">
-            </div>
-            <div>
-                <label for="email">Email:</label>
-                <input type="email" name="email" id="email">
-            </div>
-            <div>
-                <label for="firstname">First Name:</label>
-                <input type="firstname" name="firstname" id="firstname">
-            </div>
-            <div>
-                <label for="lastname">Last Name:</label>
-                <input type="lastname" name="lastname" id="lastname">
-            </div>
-            <div>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password">
-            </div>
-            <div>
-                <label for="password2">Confirm Password:</label>
-                <input type="password" name="password2" id="password2">
-            </div>
-            <div>
-                <label for="agree">
-                    <input type="checkbox" name="agree" id="agree" value="yes" /> I agree
-                    with the
-                    <a href="#" title="term of services">term of services</a>
-                </label>
-            </div>
-            <button className = 'submitbutton' type="submit">Register</button>
-            <footer>Already a member? <a href="login.php">Login here</a></footer>
-        </form>
-    </main>
+    <div class='container registrationform'>
+        <main>
+            <form action="register.php" method="post">
+                <h1>Sign Up to Corona Tracker</h1>
+                <div>
+                    <label for="username">Username:</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div>
+                    <label for="email">Email:</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div>
+                    <label for="firstname">First Name:</label>
+                    <input type="firstname" name="firstname" id="firstname">
+                </div>
+                <div>
+                    <label for="lastname">Last Name:</label>
+                    <input type="lastname" name="lastname" id="lastname">
+                </div>
+                <div>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <div>
+                    <label for="password2">Confirm Password:</label>
+                    <input type="password" name="password2" id="password2">
+                </div>
+                <div>
+                    <label for="agree">
+                        <input type="checkbox" name="agree" id="agree" value="yes" /> I agree
+                        with the
+                        <a href="#" title="term of services">term of services</a>
+                    </label>
+                </div>
+                <button className='submitbutton' type="submit">Register</button>
+                <footer>Already a member? <a href="login.php">Login here</a></footer>
+            </form>
+        </main>
+    </div>
 
 </body>
 
